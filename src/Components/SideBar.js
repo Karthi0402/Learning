e1652a6925767e0a2a7083df34be18f47a260a94
Karthi0402 <karthikgarapati123@gmail.com>
2023-03-20 20:49:28 +0530

    hh

--- src/Components/SideBar.js
+++ src/Components/SideBar.js
@@ -1,18 +1,11 @@
 import React from 'react'
 import logo from '../Img/logo1.png';
+import Upload from './upload';
 const SideBar = () => {
   return (
     <div className='sidebar fixed'>
         <a href='#' className='logo bl'><img src={logo} className='bl'/></a>
-        <ul className='nav'>
-          <li>
-            <table>
-              <tr>
-                
-              </tr>
-            </table>
-          </li>
-        </ul>
+        <Upload/>
     </div>
   )
 }
